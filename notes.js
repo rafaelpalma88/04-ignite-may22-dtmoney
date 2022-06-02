2f89be2e8c41ab644eca86466673483cfcb05b1c
Rafael Costa Palma <rafael.palma@opah.com.br>
2022-06-02 17:55:02 -0300

    including styled components

--- notes.js
+++ notes.js
@@ -3,6 +3,8 @@
 para criar um projeto atraves do boilerplate create-react-app, usar o comando:
 yarn create react-app dtmoney --template typescript
 
+instalar o styled components: yarn add styled-components
+
 
 
 
